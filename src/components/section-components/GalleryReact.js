a43dc53fb00b2abfb4f0da67097c3c2e1fbdd806
Hussain Raza <sh5raza@uwaterloo.ca>
2023-05-20 00:45:10 -0400

    images open on click

--- src/components/section-components/GalleryReact.js
+++ src/components/section-components/GalleryReact.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 import Menu from "./menu";
 
 const GalleryReact = () => {
     let publicUrl = process.env.PUBLIC_URL+'/';
   const [items, setItems] = useState(Menu);
   const [active, setActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
   const filterItem = (categItem) => {
     const updateItems = Menu.filter((curElem) => {
       return curElem.category === categItem;
@@ -14,6 +19,7 @@ const GalleryReact = () => {
     setActive(true);
   };
 
+  const images = items.map(item => item.image);
   return (
     <>
       
@@ -76,27 +82,61 @@ const GalleryReact = () => {
           </li>
         </ul>
       </div>
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % images.length)
+          }
+        />
+      )}
 
-      <div className="container-fluid mt-4">
+<div className="container-fluid mt-4">
+        <div className="row">
+          {items.map((elem, index) => {
+            const { id, name, image } = elem;
+
+            return (
+              <div className="col-sm-4" id={id} key={id}>
+                <div>
+                  <img 
+                    className="full-width-img" 
+                    src={image} 
+                    alt={name} 
+                    onClick={() => {
+                      setPhotoIndex(index);
+                      setIsOpen(true);
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      {/* <div className="container-fluid mt-4">
         <div className="row" >
           {items.map((elem) => {
             const { id, name, image } = elem;
 
             return (
               <div className="col-sm-4" id={id}>
-                {/* <div class="card mb-3"> */}
-                  {/* <div class="row no-gutters"> */}
                     <div >
     <img className="full-width-img" src={image} alt={name} />
 </div>
                     
-                  {/* </div> */}
+                 
                 </div>
-            //   </div>
             );
           })}
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
